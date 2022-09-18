bcca94db9d3031623b4126f20dc711f72c3d4dce
salman7269 <salmanvelocity5269@gmail.com>
2022-09-18 17:24:25 +0530

    next commit

--- src/main/java/Utility_classes/democlass.java
+++ src/main/java/Utility_classes/democlass.java
@@ -4,7 +4,9 @@ public class democlass {
 
 	public static void main(String[] args) {
 System.out.println("deomo class");
-System.out.println("demo class");
+
+
+System.out.println("New command");
 	}
 
 }
